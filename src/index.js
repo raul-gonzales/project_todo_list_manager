f9dcf930b72eebb2005bafd52dec6cea4c17e68e
Raul Gonzales <ragonz1108@gmail.com>
2025-07-15 17:10:54 -0500

    Deployment commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./css/styles.css";
-// import "./ui/theme.js"; // this is just for testing the color randomizer on refresh
-import { renderProjects } from "./ui/renderApp.js";
-
-try {
-  renderProjects();
-} catch (error) {
-  console.error("Error rendering app:", error);
-}
